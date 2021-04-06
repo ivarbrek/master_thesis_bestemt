cd3abeb819f41df3ad79e423be252b0bbef3a52c
Ivar Brekkå <ivarbrekka@gmail.com>
2021-04-06 11:24:19 +0200

    Update remove_consecutive_factories Avoid removing initial factory visit

--- src/alns/solution.py
+++ src/alns/solution.py
@@ -700,13 +700,14 @@ class Solution:
         for vessel, route in self.temp_routes.items():
             if len(route) < 2:
                 continue
-            next_node = route[-1]
             # iterate backwards so that we can delete without messing up indexes
-            for idx in range(len(route) - 2, -1, -1):
-                if next_node == route[idx] and self.prbl.nodes[next_node].is_factory:
-                    self.remove_node(vessel, idx)
-                else:
-                    next_node = route[idx]
+            for idx in range(len(route) - 1, 0, -1):
+                curr_node = route[idx]
+                preceding_node = route[idx - 1]
+                # remove the second visit if the route's two first factory visits are consecutive
+                remove_idx = max(1, idx)
+                if preceding_node == curr_node and self.prbl.nodes[curr_node].is_factory:
+                    self.remove_node(vessel, remove_idx)
 
     def recompute_factory_visits_route_idx(self) -> Dict[str, List[int]]:
         # infer factory visit indexes from factory visits and routes
